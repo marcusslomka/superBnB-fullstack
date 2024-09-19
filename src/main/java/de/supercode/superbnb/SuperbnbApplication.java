773e0743e13fecef92a6099546828f2864c9a4ea
marcusslomka <marcus.slomka@googlemail.com>
2024-09-19 16:13:40 +0200

    first Push, properties, .env.dev, dockerfile done

---
+++ src/main/java/de/supercode/superbnb/SuperbnbApplication.java
@@ -0,0 +1,13 @@
+package de.supercode.superbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperbnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperbnbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/supercode/superbnb/SuperbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package de.supercode.superbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperbnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
